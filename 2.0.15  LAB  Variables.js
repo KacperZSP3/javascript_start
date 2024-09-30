75b2a1014c15107aace2cbbe0b19e66ebd914fad
KacperZSP3 <k.ksykiewicz@zsp3zamosc.pl>
2024-09-30 22:12:48 +0200

    adding first tasks

---
+++ 2.0.15  LAB  Variables.js
@@ -0,0 +1,30 @@
+console.time();
+// Contact 1 - Maxwell Wright
+let name1 = "Maxwell Wright";
+let phone1 = "(0191) 719 6495";
+let email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+
+// Contact 2 - Raja Villarreal
+let name2 = "Raja Villarreal";
+let phone2 = "0866 398 2895";
+let email2 = "posuere.vulputate@sed.com";
+
+// Contact 3 - Helen Richards
+let name3 = "Helen Richards";
+let phone3 = "0800 1111";
+let email3 = "libero@convallis.edu";
+
+// Display the first and last contact details
+console.log(`${name1} / ${phone1} / ${email1}`);
+console.log(`${name3} / ${phone3} / ${email3}`);
+
+console.timeEnd();
+
+let mike = "Mike";
+let character = mike.charAt(2);
+console.log(character);
+//template string -> szablon string'a
+//umożliwia tworzenie ciągów znaków z osadzonymi wyrażeniami.
+//Aby użyć literałów szablonowych, należy użyć znaków odwrotnego apostrofu (`)
+//zamiast pojedynczych lub podwójnych cudzysłowów.
+//Następnie można uwzględnić wyrażenia wewnątrz ${} w znakach odwrotnego apostrofu.
